31fa1e348a02b4bd18d70b31a06025db5557b853
Tapopon <67146276+Tapopon@users.noreply.github.com>
2020-06-20 10:50:07 +0900

    Add files via upload

---
+++ assets/Standard Assets/Animation/Main Player/Animetor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animecontroler : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+     characterController = GetComponent<CharacterController>();
+     animator = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (characterController.isGrounded) {
+            //　左右前方の入力値をアニメーションパラメータ―に渡す
+            animator.SetFloat("ForwardSpeed", Input.GetAxis("Vertical"));
+            animator.SetFloat("LateralSpeed", Input.GetAxis("Horizontal"));
+ 
+            if (Input.GetButtonDown("Jump")) {
+                animator.SetTrigger("Jump");
+            }
+        }
+    }
+}
